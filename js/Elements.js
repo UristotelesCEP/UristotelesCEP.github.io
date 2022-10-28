9a401f0e59534c9e05150e02467e03852bad7f20
UristotelesCEP <oantrasa97@cepnet.net>
2022-10-28 12:53:31 +0200

    Mes responsive

--- js/Elements.js
+++ js/Elements.js
@@ -68,13 +68,13 @@
 		}
 		function CrearNavBar()
 		{
-			let out= `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container-fluid">
+			let out= `<nav class="navbar navbar-expand-lg navbar-light bg-info" >
+			<div class="mx-auto d-sm-flex d-block flex-sm-nowrap">
 			  <a class="navbar-brand" href="index.html">Uristoteles WEB</a>
 			  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			  </button>
-			  <div class="collapse navbar-collapse" id="navbarNav">
+			  <div class="collapse navbar-collapse text-center" id="navbarNav">
 				<ul class="navbar-nav">
 				  <li class="nav-item">
 					<a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
