558aa2b325983ca14ba621524e2024e1ec0a4f71
UristotelesCEP <oantrasa97@cepnet.net>
2022-10-28 11:03:06 +0200

    New V

---
+++ js/Data.js
@@ -0,0 +1,20 @@
+const Nombres = new Array("Primer exercisi","Dades primitives","Dades complexes","Innher Html","Document.Write","Window methods","ConsoleLog","Formularis");
+const Enlaces = new Array("https://jsfiddle.net/uristoteles/rx5sfg48/1/","https://jsfiddle.net/uristoteles/kc2buf9x/1/","https://jsfiddle.net/uristoteles/pnum4t7h/77/","https://jsfiddle.net/uristoteles/wsuneqha/","https://jsfiddle.net/uristoteles/wsuneqha/","https://jsfiddle.net/uristoteles/emo1c0q6/23/","https://jsfiddle.net/uristoteles/mh1jey6b/8/","https://jsfiddle.net/uristoteles/kmqtL5w8/158/");
+const IndexTables = new Array(1,1,1,1,1,1,1,1);
+
+const Llenguatges = new Array("");
+Llenguatges[0] = new Array("Php","JS","Css","Personal");
+Llenguatges[1] = new Array("primary","warning","info","dark");
+
+const IndexPhp = 0; //
+const IndexJS = 1;
+const IndexCss = 2;
+const IndexPersonal = 3; 
+
+const NombresGit = new Array("Pagina incial","Proyecto Pokemons","UristotelesCEP.github.io");
+const EnlacesGit = new Array("https://github.com/UristotelesCEP","https://github.com/UristotelesCEP/ExercisiPokemones","https://github.com/UristotelesCEP/UristotelesCEP.github.io");
+const IndexTables2 = new Array(3,0,3);
+
+
+
+const EnlaceImagen = "./media/GoodJfiddle.png";
--- js/Elements.js
+++ js/Elements.js
@@ -66,4 +66,85 @@
 	</div>`;
 		document.write(Out);
 		}
+		function CrearNavBar()
+		{
+			let out= `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+			  <a class="navbar-brand" href="index.html">Uristoteles WEB</a>
+			  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+				  <li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+				  </li>
+				  
+				  
+				  <li class="nav-item">
+					<a class="nav-link active" href="SecondPage.html" tabindex="-1" aria-disabled="true">MiniProgramas</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link active" href="https://github.com/UristotelesCEP" tabindex="-1" aria-disabled="true">GitHub</a>
+				  </li>
+				</ul>
+			  </div>
+			</div>
+		  </nav>`;
+		  document.write(out);
+		}
+	function CrearCardIndex(NombreCard, DescripcionCard, ImagenCard, ListaElementos, Enlaces, lenguages)
+	{
+		let out = `<div class="card" style="width: 18rem;">
+		<img class="card-img-top" src="`+ImagenCard+`" alt="Card image cap" width="300" height="300	">
+		<div class="card-body">
+		  <h5 class="card-title">`+NombreCard+`</h5>
+		  <p class="card-text">`+DescripcionCard+`</p>
+		</div>`+AfegirElementsCardIndex(ListaElementos,Enlaces,lenguages)+`</div>`;			
+	  
+
+	  document.write(out);
+	}
+	function AfegirElementsCardIndex(ListaElementos, Enlaces, lenguages)
+	{
+		let StringDeRetornoConLaLista = "";
+		StringDeRetornoConLaLista+=`<ul class="list-group">`;
+		for(cnt = 0; cnt < ListaElementos.length; cnt++){
+			let TempString = `<li class="list-group-item d-flex justify-content-between align-items-center">`+ CrearBadages(lenguages[cnt])+`<a href="`+Enlaces[cnt]+`"target="_blank">`+ListaElementos[cnt]+` </a> </li>`;
+			StringDeRetornoConLaLista += TempString;
+		}
+		StringDeRetornoConLaLista += `</ul>`;
+		return StringDeRetornoConLaLista
+
+	}
+	
+	function CrearBadages(indexNum)
+	{
+		console.log(indexNum);
+		let out;
+
+		
+
+		switch(indexNum)
+		{
+			case IndexPhp:
+				return `<span class="badge bg-`+Llenguatges[1][0]+` text-dark" style="margin-right: 10px;">`+Llenguatges[0][0]+`</span>`;
+			break;
+			case IndexJS:
+				return `<span class="badge bg-`+Llenguatges[1][1]+` text-dark" style="margin-right: 10px;">`+Llenguatges[0][1]+`</span>`;
+			break;
+			case IndexPersonal:
+				return `<span class="badge bg-`+Llenguatges[1][3]+` text-light" style="margin-right: 10px;">`+Llenguatges[0][3]+`</span>`;
+			break;
+			case IndexCss:
+				return `<span class="badge bg-`+Llenguatges[1][2]+` text-dark" style="margin-right: 10px;">`+Llenguatges[0][2]+`</span>`;
+			break;
+		}
+		
+
+		return out;
+		
+		
+		/*<span class="badge bg-warning text-dark">Warning</span>*/	
+	}
 	
