46b604cd019a548db9d86ed3233bc42e48c74622
Oriol Antras <oantrasa97@cepnet.net>
2023-03-27 13:25:58 +0200

    Cookies Example

--- js/Data.js
+++ js/Data.js
@@ -1,7 +1,7 @@
-const Nombres = new Array("Primer exercisi","Dades primitives","Dades complexes","Innher Html, Document.Write","Window methods","ConsoleLog","Formularis","Condicional If","Condicional Switch","Objeces JS - Numeros","Objeces JS - Strings","Objeces JS - Arrays","Objeces JS - Dates","Objectes - Map()", "Objectes - includes()","Arrays - Sort()");
+const Nombres = new Array("Primer exercisi","Dades primitives","Dades complexes","Innher Html, Document.Write","Window methods","ConsoleLog","Formularis","Condicional If","Condicional Switch","Objeces JS - Numeros","Objeces JS - Strings","Objeces JS - Arrays","Objeces JS - Dates","Objectes - Map()", "Objectes - includes()","Arrays - Sort()","Cookies en JS");
 
-const IndexTables = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-const jsons= new Array("1-Primer_exercisi.json","2-SegundoEjemplo.json","3-Dades_complexes.json","4-InnerHtml_i_DocumentWrite.json","5-window_methods.json","6-ConsoleLog.json","7-Formularis.json","8-IF.json","9-Switch.json","10-Number.json","11-Strings.json","12-Arrays.json","13-DateClass.json","14-MapExample.json","15-includesMethod.json","16-Sort_Method.json");
+const IndexTables = new Array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+const jsons= new Array("1-Primer_exercisi.json","2-SegundoEjemplo.json","3-Dades_complexes.json","4-InnerHtml_i_DocumentWrite.json","5-window_methods.json","6-ConsoleLog.json","7-Formularis.json","8-IF.json","9-Switch.json","10-Number.json","11-Strings.json","12-Arrays.json","13-DateClass.json","14-MapExample.json","15-includesMethod.json","16-Sort_Method.json","17-Cookies_js.json");
 
 
 
